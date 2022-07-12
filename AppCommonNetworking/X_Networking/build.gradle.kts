e18165a35f5d65ac9a15a1523ed3534cc818d387
Ivan Shlyapkin <shlyapkin01@gmail.com>
2022-07-12 21:54:49 +0300

    Up version

--- AppCommonNetworking/X_Networking/build.gradle.kts
+++ AppCommonNetworking/X_Networking/build.gradle.kts
@@ -11,14 +11,14 @@ plugins {
 
 group = "jp.co.soramitsu"
 
-version = "0.0.39"
+version = "0.0.40"
 
 publishing {
     publications {
         register<MavenPublication>("release") {
             groupId = "jp.co.soramitsu"
             artifactId = "common-networking"
-            version = "0.0.39"
+            version = "0.0.40"
 
             afterEvaluate {
                 from(components["release"])
