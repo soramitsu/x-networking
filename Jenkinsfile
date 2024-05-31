46cb876ef77cf198797db0d45848015ad6e3bad6
arvifox <arvifox@hotmail.com>
2024-05-31 11:16:22 +0300

    v1.0.1

---
+++ Jenkinsfile
@@ -0,0 +1,15 @@
+@Library('jenkins-library') _
+
+def pipeline = new org.android.ShareFeature(
+  steps: this,
+  test: true,
+  agentImage: "build-tools/android-build-box:jdk17",
+  buildCmd: 'clean build',
+  testCmd: 'test --info',
+  publishCmd: ':lib:publishAndroidReleasePublicationToScnRepoRepository',
+  sonarProjectKey: "sora:x-networking",
+  sonarProjectName: "x-networking",
+  dojoProductType: "sora-mobile"
+)
+
+pipeline.runPipeline()
---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    //trick: for the same plugin versions in all sub-modules
+    id("com.android.application").version("8.4.1").apply(false)
+    id("com.android.library").version("8.4.1").apply(false)
+    id("com.squareup.sqldelight").version("1.5.5").apply(false)
+    kotlin("android").version("1.9.24").apply(false)
+    kotlin("multiplatform").version("1.9.24").apply(false)
+    kotlin("plugin.serialization").version("1.9.24").apply(false)
+    id("org.sonarqube") version "5.0.0.4638"
+    id("com.apollographql.apollo3").version("3.8.2").apply(false)
+    id("com.google.devtools.ksp").version("1.9.24-1.0.20").apply(false)
+    id("org.jetbrains.kotlinx.kover").version("0.7.6").apply(false)
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    afterEvaluate {
+        tasks.register("testClasses") {}
+    }
+}
+
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "sora:x-networking")
+        property("sonar.projectName", "x-networking")
+        property("sonar.exclusions", "**/*.txt,**/*.kts")
+        property("sonar.coverage.jacoco.xmlReportPaths", "${project.rootDir}/report/coverage.xml")
+    }
+}
